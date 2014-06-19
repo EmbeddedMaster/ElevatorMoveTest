fa27b676c8f45ae5f3cac6dfaf013260456e2259
Hanter Jung <hanterkr@gmail.com>
2014-06-20 04:27:04 +0900

    add schedule function

--- elevator_simulator.v
+++ elevator_simulator.v
@@ -64,6 +64,10 @@ module elevator_simulator(
 	
 	reg[4:0] elv1_floor, elv2_floor;
 	reg[1:0] elv1_dir, elv2_dir;
+	reg[8:0] elv1_stop_list, elv2_stop_list;
+	
+	reg[4:0] dest;
+	reg sched_target;
 	
 	//7-segment. presentate floor
 	floor_seven_segment md_elv_floors
@@ -131,6 +135,9 @@ module elevator_simulator(
 		elv2_floor = 1;
 		elv1_dir = 1'b0;
 		elv2_dir = 1'b1;
+		
+		elv1_stop_list = 0;
+		elv2_stop_list = 0;
 	end
 
 	always @(posedge clk_in)
@@ -210,8 +217,72 @@ module elevator_simulator(
 	
 	always @(posedge input_confirm)
 	begin
-		elv1_floor = __current;
-		elv2_floor = __destination;
+		//sched_target = min(get_weight(elv1_stop_list,__current,__destination,elv1_dir),get_weight(elv2_stop_list,__current,__destination,elv2_dir));
+		dest = __destination;
+		//elv1_floor = __current;
+		//elv2_floor = __destination;
 	end
 
+	function [4:0] get_weight;
+		input [8:0] stop_list;
+		input [4:0] current;
+		input [4:0] destination;
+		input [1:0] direction;
+	begin
+		if(direction == 1)
+		begin
+			get_weight = 2*(get_max_floor(stop_list)-current) + abs(destination - current); 
+		end
+		else if(direction == 0)
+		begin
+			get_weight = 2*(current-get_min_floor(stop_list)) + abs(destination-current);
+		end
+	end
+	endfunction
+	
+	function min;
+		input [4:0] a,b;
+	begin
+		min = (a<=b)?0:1;
+	end
+	endfunction
+	
+	function abs;
+		input [4:0] a;
+	begin
+		if(a<0)
+		begin
+			abs = 0 - a;
+		end
+		else
+		begin
+			abs = a;
+		end
+	end
+	endfunction
+	
+	function [4:0] get_max_floor;
+		input [8:0] stop_list;
+		reg [4:0] i;
+	begin
+		for(i=8; i>=0; i = i-1)
+		begin
+			if(stop_list[i]==1)
+				get_max_floor = i;
+		end
+	end
+	endfunction
+	
+	function [4:0] get_min_floor;
+		input [8:0] stop_list;
+		reg [4:0] i;
+	begin
+		for(i=0; i<8; i = i+1)
+		begin
+			if(stop_list[i]==1)
+				get_min_floor = i;
+		end
+	end
+	endfunction
+	
 endmodule
